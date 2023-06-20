2bbdf0ffda319b1804c81159f99d49d635dd5c10
karthik <karthik.vbdon88@gmail.com>
2023-06-20 23:04:35 +1200

    initial commit

---
+++ mergeArrays.js
@@ -0,0 +1,24 @@
+/**
+ * You are given two integer arrays nums1 and nums2, sorted in non-decreasing order,
+ * and two integers m and n, representing the number of elements in nums1 and nums2 respectively.
+ *
+ * Merge nums1 and nums2 into a single array sorted in non-decreasing order.
+ *
+ * The final sorted array should not be returned by the function, but instead be stored inside the array nums1.
+ * To accommodate this, nums1 has a length of m + n, where the first m elements denote the elements that should be merged,
+ * and the last n elements are set to 0 and should be ignored. nums2 has a length of n.
+ */
+
+let nums1 = [1, 2, 3, 0, 0, 0],
+  m = 3,
+  nums2 = [2, 5, 6],
+  n = 3;
+
+function mergeArrays(arr1, arr2, m, n) {
+  let arr1Sliced = arr1.splice(0, m);
+  let arr2Sliced = arr2.splice(0, n);
+  let result = [...arr1Sliced, ...arr2Sliced];
+  return result.sort();
+}
+
+console.log(mergeArrays(nums1, nums2, m, n));
