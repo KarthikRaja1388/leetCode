d782ba591009cd030fe6f0197eb73aac8c2c1b19
karthik <karthik.vbdon88@gmail.com>
2023-06-21 18:52:44 +1200

    initial commit

---
+++ returnMissingNumber.js
@@ -0,0 +1,25 @@
+/**
+ * You have a set of integers s, which originally contains all the numbers from 1 to n. Unfortunately, due to some error,
+ * one of the numbers in s got duplicated to another number in the set,  which results in repetition of one number and loss of another number.
+ *
+ * You have a set of integers s, which originally contains all the numbers from 1 to n. Unfortunately, due to some error, one of the numbers
+ * in s got duplicated to another number in the set, which results in repetition of one number and loss of another number.
+ */
+
+let s = new Set([1, 2, 3, 4, 5]);
+let initalArr = Array.from(s);
+nums = [1, 5, 3, 4, 5];
+
+function findDuplicate(initialData, dataWithError) {
+  let result = [];
+
+  initalArr.filter((element) => {
+    if (!nums.includes(element)) {
+      console.log(element);
+      result.push(initalArr.indexOf(element), element);
+    }
+  });
+  return result;
+}
+
+console.log(findDuplicate(initalArr, nums));
